1adfbe07a7da0f44069b905f09870c548141a8a7
nidhi <ronaksingh2708@gmail.com>
2023-05-16 05:45:02 +0530

    url changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            "http://localhost:4000",
+            "https://chatgpt-app-pidv.onrender.com",
             { input },
             {
                 headers: {
